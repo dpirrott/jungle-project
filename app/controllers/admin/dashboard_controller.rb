a2afc73eac3b9189f3bf95cfa446e8f6bf9b51e8
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-04-06 18:17:12 -0700

    Add product and category counts to admin dash controller, then incorporate into erb

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
 
   def show
+    @productCount = Product.all.count
+    @categoryCount = Category.all.count
   end
 end

b6e519c3772ee3f8c4fc0fc9a7922be06d3e0f3f
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-04-11 18:02:07 -0700

    Implement password tests in user_spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :fullName, presence: true
+  validates :name, presence: true
   validates :email, presence: true
   validates :email, uniqueness: { case_sensitive: false }
   validates :password, presence: true
@@ -10,6 +10,6 @@ class User < ActiveRecord::Base
 
   def authenticate_with_credentials(email, password)
     user = User.find_by_email(email)
-    if user && user.authenticate(password)
+    # if user && user.authenticate(password)
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,26 @@ RSpec.describe User, type: :model do
 
   describe 'Validations' do
     # validation examples here
+    describe "Passwords" do
 
+      it "must have password filled in" do
+        @user = User.new(name: "Dylan", password_confirmation: 'xyz4', email: 'dp')
+        expect(@user.save).to be false
+      end
+      it "must have password_confirmation filled in" do
+        @user = User.new(name: "Dylan", password: 'xyz4', email: 'dp')
+        expect(@user.save).to be false
+      end
+      it "should reject if passwords don't match" do
+        @user = User.new(name: "Dylan", password: 'xyz4', password_confirmation: 'xyzb4', email: 'dp')
+        expect(@user.save).to be false
+      end
+      it "should accept if passwords match" do
+        @user = User.new(name: "Dylan", email: 'dp', password: 'xyz6', password_confirmation: 'xyz6')
+        puts @user.errors[:message]
+        expect(@user.save).to be true
+      end
+    end
   end
 
   describe '.authenticate_with_credentials' do

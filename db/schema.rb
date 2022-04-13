6a5a9e535ce6a3c9854a434dea328889166fb82d
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-04-12 23:41:37 -0700

    Complete functionality for first and last name in user registration and db

--- db/schema.rb
+++ db/schema.rb
@@ -62,7 +62,8 @@ ActiveRecord::Schema.define(version: 20220408025814) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
+    t.string   "first_name"
+    t.string   "last_name"
     t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at",      null: false

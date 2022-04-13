bfd57a480a4023422b14580d5d8e5f94c768899b
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-04-12 23:07:59 -0700

    Create foundation for first and last name support, instead of just name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :name, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :email, presence: true
   validates :email, uniqueness: { case_sensitive: false }
   validates :password, presence: true

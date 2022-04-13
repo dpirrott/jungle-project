cb80adc2ff8c2b99882018cb722da88f15241aa9
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-04-12 22:34:04 -0700

    Add product method sold_out? for use in products index view

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,12 +2,15 @@ class Product < ActiveRecord::Base
 
   monetize :price_cents, numericality: true
   mount_uploader :image, ProductImageUploader
-
   belongs_to :category
 
   validates :name, presence: true
   validates :price, presence: true
   validates :quantity, presence: true
   validates :category, presence: true
+
+  def sold_out?
+    self.quantity == 0
+  end
 
 end
